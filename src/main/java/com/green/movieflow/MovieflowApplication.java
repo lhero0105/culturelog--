1a13a073a3209591255283223181dddf62882e8e
GYEONGROK11 <gpol123@naver.com>
2023-12-06 09:59:57 +0900

    movieflow

---
+++ src/main/java/com/green/movieflow/MovieflowApplication.java
@@ -0,0 +1,13 @@
+package com.green.movieflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieflowApplication.class, args);
+    }
+
+}
