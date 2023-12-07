7a1c683bd209a6806329b855b6a4eba078e55e2a
GYEONGROK11 <gpol123@naver.com>
2023-12-07 10:03:39 +0900

    movieflow - 1

---
+++ src/main/java/com/green/movieflow/common/ResVo.java
@@ -0,0 +1,8 @@
+package com.green.movieflow.common;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ResVo {
+    private int result;
+}
---
+++ src/main/java/com/green/movieflow/media/model/InsMediaDto.java
@@ -0,0 +1,12 @@
+package com.green.movieflow.media.model;
+
+import java.util.List;
+
+public class InsMediaDto {
+    private String title;
+    private int genre;
+    private String date;
+    private int star;
+    private String contents;
+    private List<String> pics;
+}
