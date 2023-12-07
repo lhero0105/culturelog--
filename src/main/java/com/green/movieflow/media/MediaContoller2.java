72386547bad53838b0373c3d9d95d6267344a2d5
GYEONGROK11 <gpol123@naver.com>
2023-12-07 19:03:11 +0900

    movieflow - 3

---
+++ src/main/java/com/green/movieflow/media/MediaContoller2.java
@@ -0,0 +1,25 @@
+package com.green.movieflow.media;
+
+import com.green.movieflow.common.ResVo;
+import com.green.movieflow.media.model.InsMediaDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/media")
+public class MediaContoller2 {
+    private final MediaService service;
+
+    @PostMapping
+    public ResVo insMedia(@RequestBody InsMediaDto dto){
+        return null;
+    }
+
+
+}
---
+++ src/main/java/com/green/movieflow/media/MediaMapper2.java
@@ -0,0 +1,7 @@
+package com.green.movieflow.media;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface MediaMapper2 {
+}
